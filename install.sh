1aff001bad15439b88f2324adf8a1fa96fa72e37
Andreal-pixel <88157500+Andreal-pixel@users.noreply.github.com>
2022-01-02 14:40:33 +0000

    Run as root

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ echo Adgiusting...
 chmod +x /usr/local/bin/usb-lock.sh
 #Refreshing everything
 sudo udevadm control --reload
-#The End!
+#The End
 echo If you didn't encountered errors, congratulations, you can now use your usb for unlocking your pc. Remember: This script doesn't remove password
