12635066175023c386eb494aec0449ba81680f67
Andreal-pixel <88157500+Andreal-pixel@users.noreply.github.com>
2022-01-02 14:18:11 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Starting...
+#Copying files
+echo Copying Files...
+sudo cp 80-usb.rules /etc/udev/rules.d
+sudo cp usb-lock.sh /usr/local/bin
+#Making file executable
+echo Adgiusting...
+chmod +x /usr/local/bin/usb-lock.sh
+#Refreshing everything
+sudo udevadm control --reload
+#The End!
+echo If you didn't encountered errors, congratulations, you can now use your usb for unlocking your pc. Remember: This script doesn't remove password
